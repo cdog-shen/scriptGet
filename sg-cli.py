4895b1a88e00ad3ef47a740f2e37ee9dbb822d99
Shen-cdog <sc778631@163.com>
2024-08-21 12:18:37 +0800

    this is the cli main file, NOT READY yet.

---
+++ sg-cli.py
@@ -0,0 +1,13 @@
+from modules.public_import import *
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# __name__ = "test"
+
+# print(__file__)
+# print(sys.argv)
+# print(BASE_DIR)
+# print(__name__, run.__name__)
+
+# os.system()
